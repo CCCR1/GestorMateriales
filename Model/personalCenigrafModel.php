d4422eddaf904aa7b3945bb39028214ea3dff39b
JohannaC <ljcm202381@gmail.com>
2023-09-01 16:21:22 -0500

    estructura del proyecto

---
+++ Model/personalCenigrafModel.php
@@ -0,0 +1,18 @@
+<?php
+include_once('Conexion.php');
+
+class PersonalC extends Conexion
+{
+    public function __construct(){
+
+        $this->db=parent::__construct();
+       
+    }
+public function CrearPersonal()
+{
+    
+}
+    	
+}
+
+?>
