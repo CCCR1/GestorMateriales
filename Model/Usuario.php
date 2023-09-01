ee9287858e63667164507470ec1937a28933397c
CCCR1 <143225656+CCCR1@users.noreply.github.com>
2023-09-01 17:29:50 -0500

    Update Usuario.php

--- Model/Usuario.php
+++ Model/Usuario.php
@@ -1,1 +1,19 @@
-a
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class UsuarioModel extends Model
+{
+    protected $table = 'Usuario';
+    protected $primaryKey = 'idUsuario';
+    public $timestamps = false;
+
+    protected $fillable = [
+        'nombre',
+        'contrasena',
+    ];
+
+    // Agregar aquí las relaciones con otras tablas si es necesario
+}
