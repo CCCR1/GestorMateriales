ed3fc58cc95fdba5d3b78a25d23387c4df98c678
CCCR1 <143225656+CCCR1@users.noreply.github.com>
2023-09-04 16:46:03 -0500

    Update utiles.php

--- Model/utiles.php
+++ Model/utiles.php
@@ -1,13 +1,1 @@
-<?php
 
-namespace App\Utils;
-
-class Utiles
-{
-    public static function obtenerFechaYHoraActual()
-    {
-        $formato = 'Y-m-d H:i:s';
-        $fechaHoraActual = date($formato);
-        return $fechaHoraActual;
-    }
-}
