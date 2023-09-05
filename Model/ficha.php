000fe52d85caf7aed5eba4d9b8266a47fc1f098a
CCCR1 <143225656+CCCR1@users.noreply.github.com>
2023-09-05 17:32:56 -0500

    Add files via upload

---
+++ Model/ficha.php
@@ -0,0 +1,31 @@
+<?php
+
+include_once('Conexion.php');
+
+class PersonalC extends Conexion {
+
+    public function getIdFicha() {
+        return $this->idFicha;
+    }
+
+    public function setIdFicha($idFicha) {
+        $this->idFicha = $idFicha;
+    }
+
+    public function getNumeroFicha() {
+        return $this->numeroFicha;
+    }
+
+    public function setNumeroFicha($numeroFicha) {
+        $this->numeroFicha = $numeroFicha;
+    }
+
+    public function getReporte() {
+        return $this->reporte;
+    }
+
+    public function setReporte($reporte) {
+        $this->reporte = $reporte;
+    }
+}
+
---
+++ Model/programa.php
@@ -0,0 +1,25 @@
+<?php
+include_once('Conexion.php');
+
+class PersonalC extends Conexion
+    {
+
+    private $idPrograma;
+    private $nombrePrograma;
+
+    public function getIdPrograma() {
+        return $this->idPrograma;
+    }
+
+    public function setIdPrograma($idPrograma) {
+        $this->idPrograma = $idPrograma;
+    }
+
+    public function getNombrePrograma() {
+        return $this->nombrePrograma;
+    }
+
+    public function setNombrePrograma($nombrePrograma) {
+        $this->nombrePrograma = $nombrePrograma;
+    }
+}
